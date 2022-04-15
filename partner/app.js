33bff97f10e5b65e779c5d928f9944d8b5f990b3
LeVanHieu0509 <levanhieu2k1.dz@gmail.com>
2022-04-15 15:09:21 +0700

    test

--- partner/app.js
+++ partner/app.js
@@ -1,33 +1,31 @@
-var express = require('express')
-var {engine} = require('express-handlebars')
-var route = require('./routers');
+var express = require("express");
+var { engine } = require("express-handlebars");
+var route = require("./routers");
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.engine('.hbs', engine({extname: '.hbs'}));
-app.set('view engine', '.hbs');
-app.set('views', './views');
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded());
 
-
 route(app);
-app.get('/', (req, res) => {
-    res.render('getstart');
+app.get("/", (req, res) => {
+  res.render("getstart");
 });
 
+app.post("/a", (req, res) => {
+  res.send("test a post");
+});
 
-app.post('/a', (req,res) => {
-  res.send('test a post')
-})
+app.get("/xperience", (req, res) => {
+  res.render("xperience");
+});
 
-app.get('/xperience', (req,res) => {
-  res.render('xperience')
-})
-
-app.listen(3000, () => {
-  console.log('Node server running @ http://localhost:3000')
-})
+app.listen(3001, () => {
+  console.log("Node server running @ http://localhost:3001");
+});
