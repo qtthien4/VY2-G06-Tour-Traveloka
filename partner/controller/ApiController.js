6cc431f9bb8f0d532e3f480a2f72d4f599856928
qtthien4 <qtthien4@gmail.com>
2022-05-24 10:06:17 +0700

    fix api schedule

--- partner/controller/ApiController.js
+++ partner/controller/ApiController.js
@@ -130,15 +130,16 @@ class ApiController {
       var a =  arrSchedule[i].StartTime
       var timestart = new Date(a);
       console.log(timestart <= new Date());
-      //check xem qua time ch
-      if(timestart <= new Date()){
-        await schedule.update({Status:0}, { where: {IdSchedule: arrSchedule[i].IdSchedule}})
-      }
+      
       //check xem du so luong nguoi ch
       if(arrSchedule[i].AmountBooking == arrSchedule[i].Amount){
         await schedule.update({Status:0}, { where: {IdSchedule: arrSchedule[i].IdSchedule}})
       }else{
         await schedule.update({Status:1}, { where: {IdSchedule: arrSchedule[i].IdSchedule}})
+        //check xem qua time ch
+        if(timestart <= new Date()){
+        await schedule.update({Status:0}, { where: {IdSchedule: arrSchedule[i].IdSchedule}})
+      }
       }
     }
 
